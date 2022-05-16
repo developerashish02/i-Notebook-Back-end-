21647455c7bb2c7ff9ffa06d0212bdf4f38a350b
Ashish_Amar_Gaikwad <developerashish02@gmail.com>
2022-05-16 16:40:01 +0530

    storing data in mongodb using mongoose

--- db.js
+++ db.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 const mongooseURI = "mongodb://localhost:27017/?directConnection=true";
 
 const connectToMongo = async () => {
-    mongoose.connect(mongooseURI, () => {
-        console.log("connected to mongo");
-    });
+	mongoose.connect(mongooseURI, () => {
+		console.log("connected to mongo");
+	});
 };
 
 module.exports = connectToMongo;
