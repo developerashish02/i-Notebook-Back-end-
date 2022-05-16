b6d1c76fa891485ec1cd17ebbd725538c4434054
Ashish_Amar_Gaikwad <developerashish02@gmail.com>
2022-05-16 11:38:36 +0530

    setup backend of inotebook and connected to mongodb

---
+++ db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const mongooseURI = "mongodb://localhost:27017/?directConnection=true";
+
+const connectToMongo = async () => {
+    mongoose.connect(mongooseURI, () => {
+        console.log("connected to mongo");
+    });
+};
+
+module.exports = connectToMongo;
