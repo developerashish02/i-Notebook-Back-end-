60cda8da434cd8d8bab77d45152c8dd5067fb76a
Ashish_Amar_Gaikwad <developerashish02@gmail.com>
2022-05-20 22:02:32 +0530

    fix issue

--- routes/auth.js
+++ routes/auth.js
@@ -3,9 +3,9 @@ const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
 
-// create a user using :POST "/api/auth/".Doest required auth
+// create a user using :POST "/api/auth/createuser".Doest required auth
 router.post(
-	"/",
+	"/createuser",
 	[
 		body("name", "Enter a valid name").isLength({ min: 3 }),
 		body("email", "Enter a valid email").isEmail(),
@@ -13,25 +13,19 @@ router.post(
 			min: 5,
 		}),
 	],
-	(req, res) => {
+	async (req, res) => {
 		// Finds the validation errors in this request and wraps them in an object with handy functions
 		const errors = validationResult(req);
+		// if there are error return bad request and errors
 		if (!errors.isEmpty()) {
 			return res.status(400).json({ errors: errors.array() });
 		}
-		User.create({
+		// check whether the user with the same email
+		let user = await User.create({
 			name: req.body.name,
 			password: req.body.password,
 			email: req.body.email,
-		})
-			.then((user) => res.json(user))
-			.catch((errors) => {
-				console.log(errors);
-				res.json({
-					error: "Email is alredy register ",
-					message: errors.message,
-				});
-			});
+		});
 	}
 );
 
