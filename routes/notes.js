e073f41829530bf06ba524edf2696a9b773b5037
Ashish_Amar_Gaikwad <developerashish02@gmail.com>
2022-05-22 20:18:43 +0530

    create update note route

--- routes/notes.js
+++ routes/notes.js
@@ -49,4 +49,51 @@ router.post(
 	}
 );
 
+/* ROUTE:3 update notes using : PUT "/api/auth/updatenote" Login requreid */
+
+router.put(
+	"/updatenote/:id",
+	fetchuser,
+	// [
+	//     body("title", "Enter a valid title").isLength({ min: 3 }),
+	//     body("description", "Description must be 5 character").isLength({ min: 5 }),
+	// ],
+	async (req, res) => {
+		try {
+			const { title, description, tag } = req.body;
+			// create a newNote object
+			const newNote = {};
+			if (title) {
+				newNote.title = title;
+			}
+			if (description) {
+				newNote.description = description;
+			}
+			if (tag) {
+				newNote.tag = tag;
+			}
+
+			// find the note to be updated and update
+			let note = await Note.findById(req.params.id);
+			if (!note) {
+				return res.status(404).send("Not found");
+			}
+
+			if (note.user.toString() !== req.user.id) {
+				return res.status(401).send("Not Allowed");
+			}
+
+			note = await Note.findByIdAndUpdate(
+				req.params.id,
+				{ $set: newNote },
+				{ new: true }
+			);
+			res.json(note);
+		} catch (error) {
+			console.error(error.message);
+			res.send(500).send("internal Server Error");
+		}
+	}
+);
+
 module.exports = router;
